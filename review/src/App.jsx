611c3f40fd22d1c3e2ca0df3b8b44078122521cd
MLDODSON <paola@dominicansoul.com>
2024-01-20 16:01:29 -0600

    Branch 1
    Branch 1 - Reviews project setup.

---
+++ review/src/App.jsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h1>Reviews</h1>
+  )
+};
+
+export default App;
---
+++ review/src/data.js
@@ -0,0 +1,47 @@
+const reviews = [
+    {
+        id: 1,
+        name: "dave casper",
+        image: "https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_1280.jpg",
+        job: "graphic designer",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+    },
+    {
+        id: 2,
+        name: "lucy pearl",
+        image: "https://previews.dropbox.com/p/thumb/ACJyPgFlNuJAC3TNMbbdSvbmdA4gS9DQ0CLeXZa3R4HAv6tbxLl2-j5roehbcTmorPzwbBgDCL_QD-QYUwAL8P74m2h2srzm2p7VZevej_8_zdeA3rvjv9bWZOi7d9kW7e8gE3DaVjWtxR4W4lh3NxUg6BIOwCW5V2FDgYgJOxmG4i4GAeH1S-fJuQaW7FKnnZsCV9uIDpxdHWiEQHrthsQgTxpBGOjYMTz46yASr-_AO6IQ6wGXSEIyY3SCoXUEh86O4lr_zyXIZd356egandSRSpxE2R0Ccgt3BLzQEOrdSJtTCmP7shgbtuCtx91OUjOu6mEkJz9EZJwNFqESP3gO/p.jpeg",
+        job: "social media",
+        text: "Dolor sit amet, consectetur adipiscing elit. Nullam ac urna et elit fermentum volutpat. Vivamus non condimentum justo. Integer vel laoreet odio. Suspendisse potenti."
+    },
+    {
+        id: 3,
+        name: "scotty pickles",
+        image: "https://cdn.pixabay.com/photo/2014/05/30/17/37/hipster-358479_1280.jpg",
+        job: "web developer",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum odio a consectetur posuere. Integer dapibus, nulla ut commodo luctus, libero velit consectetur."
+    },
+    {
+        id: 4,
+        name: "wendy woo",
+        image: "https://cdn.pixabay.com/photo/2020/09/18/15/38/woman-5582147_1280.jpg",
+        job: "web designer",
+        text: "Dolor sit amet, consectetur adipiscing elit. Fusce ac tristique odio. Praesent euismod justo ac sapien congue, in ullamcorper nunc malesuada. Integer in ipsum at lorem volutpat lacinia."
+    },
+    {
+        id: 5,
+        name: "star bright",
+        image: "https://cdn.pixabay.com/photo/2022/10/19/19/52/woman-7533393_1280.jpg",
+        job: "marketing",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum cursus felis, nec eleifend metus luctus vel. Nulla facilisi. Vivamus ac ante nec velit varius ullamcorper."
+    },
+    {
+        id: 6,
+        name: "jack hammer",
+        image: "https://previews.dropbox.com/p/thumb/ACJ7gmYUS426RySJBqtZOcxll1OUo9YXkyLMTTu_3KHWorLIZQfGv6KtaMmvAP7xjI7i7uKgkPKz_DGWpEgaOANlrtwc7YNL89-wZ5hXkP2r3tzg8kHDvrm1aDl89zWeDcCtl2QXz5LNZ5q2V1i3C13TAM3IBcQhX2a73vmWyzaQ9NCInDrIDgciYVwXpYml1K4UfyLb6B3A4mvepG-WIXCjHxUWzsirCbQgwE_ks_REN-4w5am6uxK9XIzIC_II4r5awjtZ-799JW93uLROH4sTj4iu4Jun4bNIiimloXo8JHB9XrUkLQKlzSomXpo4QuFOZ3-XqwOs_HkqvlXEy3vc/p.jpeg",
+        job: "the boss",
+        text: "Dolor sit amet, consectetur adipiscing elit. Quisque nec urna non tortor fringilla fringilla. In hac habitasse platea dictumst. Integer ut metus sit amet tortor efficitur consequat."
+    },
+];
+
+export default reviews;
+
